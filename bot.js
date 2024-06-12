d267791664b8ec005c93c00c5e7ed92e9cca469c
Nick Chan <nick.chan@utexas.edu>
2024-06-12 10:23:59 -0500

    Adjustable env file

--- bot.js
+++ bot.js
@@ -2,11 +2,12 @@ const { Client, Collection, Events, GatewayIntentBits, Partials } = require("dis
 const assert = require("assert");
 const path = require("path");
 const findEvents = require("./src/utils/initialization/findEvents");
+const registerInteractions = require("./src/utils/initialization/registerInteractions");
 const findCommands = require("./src/utils/initialization/findCommands");
 const registerCommands = require("./src/utils/initialization/registerCommands");
-const registerInteractions = require("./src/utils/initialization/registerInteractions");
 
-require("dotenv").config({ path: path.join(__dirname, ".env.test") });
+const envFile = ".env";
+require("dotenv").config({ path: path.join(__dirname, envFile) });
 assert(process.env.TOKEN, "A Discord bot token for is required.");
 
 const client = new Client({
